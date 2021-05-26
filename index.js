6a9ee2b886f91f705bcefe9bf16c4d986a1c64fd
megan <megan.schertz@gmail.com>
2021-05-25 20:29:49 -0500

    commit tree 7a1f7a94d38179aad41eb69be4575af796d6bd23

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '')
+  }
+
+  static titleize(string) {
+    const noCapital = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    const title = []
+    string = string.toLowerCase().split(' ')
+    for(let i = 0; i < string.length; i++){
+      if (i === 0 || !noCapital.includes(string[i])){
+        title.push(this.capitalize(string[i]))
+      } else {
+        title.push(string[i])
+      }
+    }
+    return title.join(' ')
+  } 
 }
